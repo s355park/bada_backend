fb67ed0a418f5a15b300bbead51801498ab2b538
Sanghyun Park <s355park@uwaterloo.ca>
2023-09-05 04:19:28 -0700

    init

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,15 @@
 const { firstAccountCreation } = require("./db");
 const mongoose = require("mongoose");
 const { connect } = require("./connect");
+const Account = require("./models/accountSchema")
 
 const test = async() => {
     const connection = await connect(mongoose);
+
+    await firstAccountCreation(
+        connection,
+        
+    );
+
     
 }
--- src/db/models/accountSchema.js
+++ src/db/models/accountSchema.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("../connect");
 const schema = mongoose.Schema;
 
 const accountSchema = new mongoose.Schema({
